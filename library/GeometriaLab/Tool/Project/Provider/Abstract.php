aca63f1ca2040235bb349f542215ebcbebb9d2b4
Ivan Shumkov <ivan@shumkov.ru>
2011-12-02 10:50:40 +0300

    implements storage, move config and storage to .zf

--- library/GeometriaLab/Tool/Project/Provider/Abstract.php
+++ library/GeometriaLab/Tool/Project/Provider/Abstract.php
@@ -2,6 +2,11 @@
 
 class GeometriaLab_Tool_Project_Provider_Abstract extends Zend_Tool_Project_Provider_Abstract
 {
+    /**
+     * @var Zend_Tool_Framework_Client_Storage
+     */
+    protected $_storage;
+
     public function getName()
     {
         $className = get_class($this);
@@ -14,5 +19,21 @@ class GeometriaLab_Tool_Project_Provider_Abstract extends Zend_Tool_Project_Prov
             $providerName = substr($providerName, 0, strlen($providerName)-8);
         }
         return $providerName;
+    }
+
+    protected function _getStorage()
+    {
+        if (null == $this->_storage) {
+            $path = APPLICATION_PATH . '/../.zf/storage';
+            if (!is_dir($path)) {
+                mkdir($path);
+            }
+            $adapter = new Zend_Tool_Framework_Client_Storage_Directory($path);
+            $this->_registry->getStorage()->setAdapter($adapter);
+
+            $this->_storage = $this->_registry->getStorage();
+        }
+
+        return $this->_storage;
     }
 }
