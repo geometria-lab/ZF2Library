ee2baab3fe31d97b2fbd0b451357aa61bfba4f1c
Ivan Shumkov <ivan@shumkov.ru>
2011-12-12 10:16:11 +0300

    fix test

--- tests/models/User.php
+++ tests/models/User.php
@@ -1,13 +1,10 @@
 <?php
 
 /**
- * @property        $id
  * @property string $firstName  { "defaultValue" : "Ivan" }
  * @property string $secondName { "defaultValue" : "Shumkov" }
- *
- * @mapper mongo { "connection" : "default", "collection" : "UserName" }
  */
-class User extends GeometriaLab_Model_Persistent
+class User extends GeometriaLab_Model
 {
 
 }
