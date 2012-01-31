c9daa82f4ff3e7f0eeffbe1a623d53e076b2de06
Ivan Shumkov <ivan@shumkov.ru>
2012-01-31 10:19:18 +0300

    fix vBucket map

--- library/GeometriaLab/VBuckets/Map/Config.php
+++ library/GeometriaLab/VBuckets/Map/Config.php
@@ -5,17 +5,21 @@ class GeometriaLab_VBuckets_Map_Config implements GeometriaLab_VBuckets_Map_Inte
     /**
      * Config
      *
-     * @var arra
+     * @var array
      */
     protected $_config;
 
     /**
      * Constructor
      *
-     * @param Zend_Config $config
+     * @param array|Zend_Config $config
      */
-    public function __construct(Zend_Config $config)
+    public function __construct($config)
     {
+        if ($config instanceof Zend_Config) {
+            $config = $config->toArray();
+        }
+
         $this->_config = $this->_validate($config);
     }
 
@@ -27,9 +31,9 @@ class GeometriaLab_VBuckets_Map_Config implements GeometriaLab_VBuckets_Map_Inte
      */
     public function getVBucket($id)
     {
-        foreach($this->_getConfig()->ranges as $maxId => $range) {
+        foreach($this->_config['ranges'] as $maxId => $range) {
             if ($maxId >= $id) {
-                return new GeometriaLab_VBuckets_Bucket($id, $range->toArray());
+                return new GeometriaLab_VBuckets_Bucket($id, $range);
             }
         }
 
@@ -43,50 +47,44 @@ class GeometriaLab_VBuckets_Map_Config implements GeometriaLab_VBuckets_Map_Inte
      */
     public function getVBucketsCount()
     {
-        return (integer)$this->_getConfig()->count;
+        return $this->_config['count'];
     }
 
     /**
      * Validate config
      *
      * @throws GeometriaLab_VBuckets_Map_Exception
-     * @param Zend_Config $config
-     * @return Zend_Config
+     * @param array $config
+     * @return array
      */
-    protected function _validate(Zend_Config $config)
+    protected function _validate($config)
     {
-        if (!isset($config->count) ||
-            (integer)$config->count != $config->count ||
-            (integer)$config->count < 1) {
+        if (!isset($config['count']) ||
+            (integer)$config['count'] != $config['count'] ||
+            (integer)$config['count'] < 1) {
             throw new GeometriaLab_VBuckets_Map_Exception('Undefined vBuckets count');
         }
 
-        if (!isset($config->ranges) || count($config->ranges) == 0) {
+        $config['count'] = (integer)$config['count'];
+
+        if (!isset($config['ranges']) || count($config['ranges']) == 0) {
             throw new GeometriaLab_VBuckets_Map_Exception('Ranges not present');
         }
 
+        ksort($config['ranges']);
+
         $lastRangeMaxId = 0;
-        foreach($config->ranges as $maxId => $range) {
-            if (!is_int($maxId) || $maxId < 1 || $maxId > (integer)$config->count) {
+        foreach($config['ranges'] as $maxId => $range) {
+            if (!is_int($maxId) || $maxId < 1 || $maxId > $config['count']) {
                 throw new GeometriaLab_VBuckets_Map_Exception('Invalid range');
             }
             $lastRangeMaxId = $maxId;
         }
 
-        if ($lastRangeMaxId != (integer)$config->count) {
+        if ($lastRangeMaxId != $config['count']) {
             throw new GeometriaLab_VBuckets_Map_Exception('vBuckets count are not covered by ranges');
         }
 
         return $config;
-    }
-
-    /**
-     * Get config
-     *
-     * @return Zend_Config
-     */
-    protected function _getConfig()
-    {
-        return $this->_config;
     }
 }
