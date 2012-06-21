f4e1a23a6738520165f60d1a9843cb4e14e49e75
Ivan Shumkov <ivan@shumkov.ru>
2012-06-22 01:46:01 +0400

    add some tests

--- tests/GeometriaLab/Model/Persistent/ModelTest.php
+++ tests/GeometriaLab/Model/Persistent/ModelTest.php
@@ -6,15 +6,96 @@ use GeometriaLabTest\Model\Persistent\Models\PersistentModel,
     GeometriaLabTest\Model\Persistent\Models\PersistentModelWithInvalidDefinition,
     GeometriaLabTest\Model\Persistent\Models\PersistentModelWithInvalidDefinition2,
     GeometriaLabTest\Model\Persistent\Models\PersistentModelWithInvalidDefinition3,
+    GeometriaLabTest\Model\Persistent\Models\PersistentModelWithoutDefinition,
     GeometriaLabTest\Model\Models\SubModel;
 
 
 class ModelTest extends \PHPUnit_Framework_TestCase
 {
-    public function setUp()
-    {
+    /**
+     * Save model to storage
+     *
+     * @return boolean
+     */
+    public function save();
 
-    }
+    /**
+     * Delete model from storage
+     *
+     * @return boolean
+     */
+    public function delete();
+
+    /**
+     * Is not saved model
+     *
+     * @return boolean
+     */
+    public function isNew();
+
+    /**
+     * Is model changed
+     *
+     * @return boolean
+     */
+    public function isChanged();
+
+    /**
+     * Is property changed
+     *
+     * @param string $name
+     * @return boolean
+     */
+    public function isPropertyChanged($name);
+
+    /**
+     * Get changed property
+     *
+     * @return array
+     */
+    public function getChangedProperties();
+
+    /**
+     * Get property change
+     *
+     * @param string $name
+     * @return array
+     */
+    public function getChange($name);
+
+    /**
+     * Get model changes
+     *
+     * @return array
+     */
+    public function getChanges();
+
+    /**
+     * Get clean property value
+     *
+     * @param string $name
+     * @return mixed
+     */
+    public function getClean($name);
+
+    /**
+     * Mark model as clean
+     *
+     * @param boolean $flag
+     * @return ModelInterface
+     */
+    public function markClean($flag = true);
+
+    /**
+     * Get mapper
+     *
+     * @static
+     * @return Mapper\MapperInterface
+     */
+    static public function getMapper();
+
+
+
 
     public function testGetMapper()
     {
@@ -24,22 +105,25 @@ class ModelTest extends \PHPUnit_Framework_TestCase
 
     public function testGetMapperWithoutDefinition()
     {
-
+        $this->setExpectedException('\InvalidArgumentException');
+        PersistentModelWithoutDefinition::getMapper();
     }
 
     public function testGetMapperWithInvalidDefinition()
     {
+        $this->setExpectedException('\InvalidArgumentException');
         PersistentModelWithInvalidDefinition::getMapper();
     }
 
     public function testGetMapperWithInvalidDefinition2()
     {
+        $this->setExpectedException('\InvalidArgumentException');
         PersistentModelWithInvalidDefinition2::getMapper();
     }
 
     public function testGetMapperWithInvalidDefinition3()
     {
-
+        $this->setExpectedException('\InvalidArgumentException');
         PersistentModelWithInvalidDefinition3::getMapper();
     }
 
