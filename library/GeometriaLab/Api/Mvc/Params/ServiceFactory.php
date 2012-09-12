5409dedba1594e76bbf74d33aa56f252d80043ea
Nikolay Bukarinov <bukarinov@gmail.com>
2012-09-12 14:56:45 +0400

    Rename ParamsLoader to ServiceFactory and move it from Service to Params

---
+++ library/GeometriaLab/Api/Mvc/Params/ServiceFactory.php
@@ -0,0 +1,74 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: max
+ * Date: 30.07.12
+ * Time: 16:15
+ * To change this template use File | Settings | File Templates.
+ */
+
+namespace GeometriaLab\Api\Mvc\Params;
+
+use GeometriaLab\Api\Mvc\Controller\Action\Params;
+
+use Zend\Mvc\Router\RouteMatch as ZendRouteMatch,
+    Zend\ServiceManager\FactoryInterface as ZendFactoryInterface,
+    Zend\ServiceManager\ServiceLocatorInterface as ZendServiceLocatorInterface;
+
+class ServiceFactory implements ZendFactoryInterface
+{
+    /**
+     * @var string
+     */
+    private $paramsNamespace;
+
+    /**
+     * @param ZendServiceLocatorInterface $serviceLocator
+     * @return Params
+     * @throws \InvalidArgumentException
+     */
+    public function createService(ZendServiceLocatorInterface $serviceLocator)
+    {
+        $config = $serviceLocator->get('Configuration');
+
+        if (!isset($config['params'])) {
+            throw new \InvalidArgumentException('Need not empty "params" param in config');
+        }
+        if (!isset($config['params']['__NAMESPACE__'])) {
+            throw new \InvalidArgumentException('Need not empty "params.__NAMESPACE__" param in config');
+        }
+
+        $this->setNamespace($config['params']['__NAMESPACE__']);
+
+        /* @var ZendRouteMatch $routeMatch */
+        $routeMatch= $serviceLocator->get('Application')->getMvcEvent()->getRouteMatch();
+
+        return $this->getByRouteMatch($routeMatch);
+    }
+
+    /**
+     * @param string $nameSpace
+     * @return ServiceFactory
+     */
+    public function setNamespace($nameSpace)
+    {
+        $this->paramsNamespace = $nameSpace;
+        return $this;
+    }
+
+    /**
+     * @param ZendRouteMatch $routeMatch
+     * @return Params
+     * @throws \InvalidArgumentException
+     */
+    public function getByRouteMatch(ZendRouteMatch $routeMatch)
+    {
+        if ($this->paramsNamespace === null) {
+            throw new \InvalidArgumentException('Need setup Namespace');
+        }
+
+        $paramClassName = $this->paramsNamespace . '\\' . $routeMatch->getParam('__CONTROLLER__') . '\\' . ucfirst($routeMatch->getParam('action'));
+
+        return new $paramClassName();
+    }
+}
