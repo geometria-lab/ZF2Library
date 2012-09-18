4c03b9ac6ebff094c025627ef72a20913e846552
Nikolay Bukarinov <bukarinov@gmail.com>
2012-09-18 16:21:56 +0400

    No prepare for relation properties

--- library/GeometriaLab/Model/Persistent/Schema/Property/Relation/BelongsTo.php
+++ library/GeometriaLab/Model/Persistent/Schema/Property/Relation/BelongsTo.php
@@ -9,13 +9,4 @@ class BelongsTo extends AbstractRelation
     protected $targetProperty = 'id';
 
     protected $relationClass = '\GeometriaLab\Model\Persistent\Relation\BelongsTo';
-
-    public function prepare($value)
-    {
-        if (!$value instanceof ModelInterface) {
-            throw new \InvalidArgumentException('must implement GeometriaLab\Model\Persistent\ModelInterface');
-        }
-
-        return $value;
-    }
 }
--- library/GeometriaLab/Model/Persistent/Schema/Property/Relation/HasOne.php
+++ library/GeometriaLab/Model/Persistent/Schema/Property/Relation/HasOne.php
@@ -7,13 +7,4 @@ use GeometriaLab\Model\Persistent\ModelInterface;
 class HasOne extends AbstractHasRelation
 {
     protected $relationClass = '\GeometriaLab\Model\Persistent\Relation\HasOne';
-
-    public function prepare($value)
-    {
-        if (!$value instanceof ModelInterface) {
-            throw new \InvalidArgumentException('must implement GeometriaLab\Model\Persistent\ModelInterface');
-        }
-
-        return $value;
-    }
 }
