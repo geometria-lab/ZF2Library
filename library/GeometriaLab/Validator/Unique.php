bfcba2b20d0696fdbe6b1cae62f9ef2dd76a7af8
Nikolay Bukarinov <bukarinov@gmail.com>
2012-10-10 11:44:04 +0400

    Unique validator.

---
+++ library/GeometriaLab/Validator/Unique.php
@@ -0,0 +1,95 @@
+<?php
+
+namespace GeometriaLab\Validator;
+
+use GeometriaLab\Model\Persistent\AbstractModel;
+
+use Zend\Validator\AbstractValidator as ZendAbstractValidator,
+    Zend\Validator\Exception\RuntimeException as ZendRuntimeException,
+    Zend\Validator\Exception\InvalidArgumentException as ZendInvalidArgumentException;
+
+class Unique extends ZendAbstractValidator
+{
+    const EXISTS = 'exists';
+
+    /**
+     * Error messages
+     *
+     * @var array
+     */
+    protected $messageTemplates = array(
+        self::EXISTS => "'%value%' already exists",
+    );
+    /**
+     * Model class name for validation
+     *
+     * @var string
+     */
+    protected $class;
+    /**
+     * Field name for validation
+     *
+     * @var string
+     */
+    protected $field;
+
+    /**
+     * Set model class name
+     *
+     * @param string $class
+     * @return Unique
+     * @throws ZendInvalidArgumentException
+     */
+    public function setClass($class)
+    {
+        if (!is_subclass_of($class, '\GeometriaLab\Model\Persistent\AbstractModel')) {
+            throw new ZendInvalidArgumentException('Class mus be instance of \GeometriaLab\Model\Persistent\AbstractModel');
+        }
+
+        $this->class = $class;
+
+        return $this;
+    }
+
+    /**
+     * Set field name
+     *
+     * @param string $field
+     * @return Unique
+     */
+    public function setField($field)
+    {
+        $this->field = $field;
+        return $this;
+    }
+
+    /**
+     * @param mixed $value
+     * @return bool
+     * @throws ZendRuntimeException
+     */
+    public function isValid($value)
+    {
+        if ($this->class === null) {
+            throw new ZendRuntimeException("Class not configured");
+        }
+
+        if ($this->field === null) {
+            throw new ZendRuntimeException("Field not configured");
+        }
+
+        // @TODO Remove Unique validator from Validator Chain, then match count, then add Unique validator to chain
+
+        /* @var AbstractModel $model */
+        /*$model = new $this->class;
+        $matchedCount = $model->getMapper()->count(array($this->field => $value));
+
+        if ($matchedCount) {
+            $this->error(self::EXISTS, $value);
+            return false;
+        }
+        */
+
+        return true;
+    }
+}
