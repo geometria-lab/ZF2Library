1015a12dc7c05200cfb217da6b357035baedee81
Nikolay Bukarinov <bukarinov@gmail.com>
2012-11-06 19:33:07 +0400

    Rename @STUB to @TODO

--- library/GeometriaLab/Api/Mvc/Controller/Action/Params/Listener.php
+++ library/GeometriaLab/Api/Mvc/Controller/Action/Params/Listener.php
@@ -92,7 +92,7 @@ class Listener implements ZendListenerAggregateInterface
                 unset($params[$key]);
             }
         }
-        // @STUB It's possible to remove the access_token param only here and nowhere else
+        // @TODO It's possible to remove the access_token param only here and nowhere else
         unset($params['access_token']);
 
         return $params;
