6bc1552690e06424651e9e0d3d9d3be6370d3258
Nikolay Bukarinov <bukarinov@gmail.com>
2012-11-20 17:40:03 +0400

    Test refactoring

--- library/GeometriaLab/Test/PHPUnit/Controller/AbstractHttpControllerTestCase.php
+++ library/GeometriaLab/Test/PHPUnit/Controller/AbstractHttpControllerTestCase.php
@@ -6,6 +6,7 @@ use Zend\Test\PHPUnit\Controller\AbstractHttpControllerTestCase as ZendAbstractH
 
 /**
  * @method \Zend\Http\PhpEnvironment\Request getRequest()
+ * @method \Zend\Http\PhpEnvironment\Response getResponse()
  */
 class AbstractHttpControllerTestCase extends ZendAbstractHttpControllerTestCase
 {
@@ -36,9 +37,33 @@ class AbstractHttpControllerTestCase extends ZendAbstractHttpControllerTestCase
     {
         $message = ($message) ?: sprintf('Failed asserting that JSON path "%s" value equals "%s"', $path, $expected);
 
+        $this->assertContentType('application/json');
+
         $actual = $this->getJsonValueByPath($path);
 
         self::assertEquals($expected, $actual, $message, $delta, $maxDepth, $canonicalize, $ignoreCase);
+    }
+
+    /**
+     *  Assert that content-type header and $expected are equals
+     *
+     * @param string $expected
+     * @param string $message
+     * @param int $delta
+     * @param int $maxDepth
+     * @param bool $canonicalize
+     * @param bool $ignoreCase
+     * @throws \PHPUnit_Framework_AssertionFailedError
+     */
+    protected function assertContentType($expected, $message = '', $delta = 0, $maxDepth = 10, $canonicalize = false, $ignoreCase = false)
+    {
+        $contentType = $this->getResponse()->getHeaders()->get('content-type');
+
+        if ($contentType === false) {
+            throw new \PHPUnit_Framework_AssertionFailedError('Content-type not found in Headers');
+        }
+
+        self::assertEquals($expected, $contentType->getFieldValue(), $message, $delta, $maxDepth, $canonicalize, $ignoreCase);
     }
 
     /**
@@ -52,6 +77,7 @@ class AbstractHttpControllerTestCase extends ZendAbstractHttpControllerTestCase
     {
         $path = trim($path, '/');
         $sections = explode('/', $path);
+
         $json = $this->getJsonFromResponse();
 
         if (!is_array($json)) {
